8507367672e7bde296969809ca8ac55e936b02e1
Vasanthakumaran <gvasanthakumaran@gmail.com>
2017-04-25 20:02:21 +0530

    Creating source file to find string length

---
+++ StringLength.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+ 	string s="hi";
+ 	//cin>>s;
+ 	//cout<<s<<endl;
+ 	int x;
+//  	x=strlen(s);
+ 	//cout<<x<<endl;
+ 	int y=sizeof(s);
+ 	cout<<y<<endl;
+ 	system("pause");
+ 	return 0;
+}
